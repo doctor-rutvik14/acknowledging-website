5f61e7cef114bd36b512ed0c2f2ec2d36eb59465
doctor-rutvik14 <rutvik14d@gmail.com>
2020-08-17 19:43:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>{
+  let curDate = new Date();
+  curDate = curDate.getHours();
+  let ack='';
+  let Style ={}
+  if(curDate >=1 && curDate < 12)
+  {
+    ack = 'Good Morning';
+    Style.color = "#388378"
+  } 
+  else if(curDate >=12 && curDate < 17){
+    ack = 'Good Afternoon';
+    Style.color = "yellow"
+  }
+  else if(curDate >=17 && curDate < 20){
+    ack = 'Good Evening';
+    Style.color = "orange"
+  }
+  else{
+    ack = 'Good Night';
+  }
+  return(
+    <>
+      <h1>Hello, <span style={Style}> {ack} </span></h1>
+    </>
   );
-}
-
+} 
 export default App;
